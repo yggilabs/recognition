47e2bf18d4c54004e0ebdca564fd50930c87a580
yggilabs <skelley.android@gmail.com>
2023-12-30 15:38:14 +0900

    seeded random and bias fix
    some cleanup too

--- script.js
+++ script.js
@@ -8,43 +8,160 @@ const COLOR_UNKNOWN = 6,
 	COLOR_ORANGE = 4,
 	COLOR_YELLOW = 5;
 
-
 const params = new URLSearchParams(document.location.search);
 
+const array_to_pattern = array => {
+  const pattern = {
+    nw: array[0],
+    ne: array[1],
+    sw: array[2],
+    se: array[3],
+  };
+
+  return pattern;
+}
+
+const pattern_append_id = (pattern, id) => {
+  pattern.id = id;
+
+  return pattern;
+}
+
 // parse url params for pattern definitions
-const base_patterns = params.get("patterns").split("").reduce((a,c) => {
+const base_patterns = params.get("patterns").split("").reduce((a,c) => { // monkaCodeStart
   a.c.push(parseInt(c))
   if(a.c.length == 4) {
     a.a.push(a.c)
     a.c = [];
   }
   return a;
-},{a:[],c:[]}).a.map((a,i) => {
-  return {
-    id: i,
-    nw: a[0],
-    ne: a[1],
-    sw: a[2],
-    se: a[3],
-  };
-});
+},{a:[],c:[]}).a // monkaCodeEnd
+.map(array_to_pattern).map(pattern_append_id);
 
 // parse url for board
 const base_board = params.get("board").split("").map(i => parseInt(i));
+const board_width = parseInt(params.get("width")) || 10;
+const board_height = parseInt(params.get("height")) || 10;
+const cell_size = parseInt(params.get("size")) || 20;
+const random_seed = params.get("seed");
 
-const BOARD_WIDTH = parseInt(params.get("width")) || 10;
-const BOARD_HEIGHT = parseInt(params.get("height")) || 10;
-const cell_size = parseInt(params.get("size")) || 40;
+const random = random_seed === undefined ? new Math.seedrandom() : new Math.seedrandom(random_seed); // use seed param for deterministic mode or local entropy for non-deterministic mode
 
 const root = document.querySelector(':root');
 
-root.style.setProperty('--column-count', BOARD_WIDTH);
-root.style.setProperty('--row-count', BOARD_HEIGHT);
+root.style.setProperty('--column-count', board_width);
+root.style.setProperty('--row-count', board_height);
 root.style.setProperty('--size', `${cell_size}mm`);
 
 /*
 https://yggilabs.github.io/recognition/?patterns=1523225133555211411211412322524453121154&board=6660000666660000006660002004060300000000000000100000030000200000000000600000040666050000666660000666
 */
+
+const next_board = board => {
+
+  const board_to_frames = board => {
+    const frames = []
+    
+    for(let i = 0; i < board_height - 1; i++) {
+      for(let j = 0; j < board_width - 1; j++) {
+        // calculate positions
+        const nw_index = (board_width*i)+j;
+        const ne_index = nw_index + 1;
+        const sw_index = nw_index + board_width;
+        const se_index = sw_index + 1;
+    
+        const nw_value = board[nw_index];
+        const ne_value = board[ne_index];
+        const sw_value = board[sw_index];
+        const se_value = board[se_index];
+        
+        // lookup values
+        const frame = {
+          nw: {
+            value: nw_value,
+            index: nw_index
+          },
+          ne: {
+            value: ne_value,
+            index: ne_index
+          },
+          sw: {
+            value: sw_value,
+            index: sw_index
+          },
+          se: {
+            value: se_value,
+            index: se_index
+          }
+        }
+
+        frames.push(frame);
+      }
+    }
+
+    return shuffle(frames); // removes some bias from testing
+  }
+
+  const frame_is_valid = frame => 
+    (frame.nw.value >= 0 && frame.nw.value < 6) && // all values are (0, 6]
+    (frame.ne.value >= 0 && frame.ne.value < 6) &&
+    (frame.sw.value >= 0 && frame.sw.value < 6) &&
+    (frame.se.value >= 0 && frame.se.value < 6) &&
+    (frame.nw.value == 0 || frame.ne.value == 0 || frame.sw.value == 0 || frame.se.value == 0) // AND some values are 0
+
+  const frames = board_to_frames(board).filter(frame_is_valid);
+
+  const pattern_matches_frame = frame => {
+    return pattern => 
+      frame.nw.value == 0 | frame.nw.value == pattern.nw &&
+      frame.ne.value == 0 | frame.ne.value == pattern.ne &&
+      frame.sw.value == 0 | frame.sw.value == pattern.sw &&
+      frame.se.value == 0 | frame.se.value == pattern.se
+
+  }
+
+  const sort_bias_desc = (a, b) => (id_counter[a.id] || 0) - (id_counter[b.id] || 0);
+	const has_entropy = a => a.entropy > 0;
+  const sort_entropy_asc = (a, b) => a.entropy - b.entropy;
+  const calculate_entropy = frame => {
+    const matches = expanded_patterns.filter(pattern_matches_frame(frame));
+	frame.matches = matches;
+    frame.entropy = matches.length;	
+    return frame;
+  };
+  
+  const candidates = frames.map(calculate_entropy).filter(has_entropy).sort(sort_entropy_asc);
+    const selected = candidates[0];
+
+  
+
+  if(selected === undefined) return board; // if there are no candidates, try again
+  
+  // apply selected to board
+
+  const matches = shuffle(selected.matches).sort(sort_bias_desc);
+  const match = matches[0];
+  
+  nw_index = selected.nw.index;
+  ne_index = selected.ne.index;
+  sw_index = selected.sw.index;
+  se_index = selected.se.index;
+  
+  nw_value = match.nw;
+  ne_value = match.ne;
+  sw_value = match.sw;
+  se_value = match.se;
+  
+  board[nw_index] = nw_value;
+  board[ne_index] = ne_value;
+  board[sw_index] = sw_value;
+  board[se_index] = se_value;
+
+
+  id_counter[match.id] = 1 + (id_counter[match.id] || 0); // track match id
+
+  return board;
+}
 
 // rotate a pattern "clockwise"
 const rotate_pattern = pattern => {
@@ -83,131 +200,20 @@ const expand_pattern_rotate = pattern => {
 	return rotations;
 };
 
-const next_board = board => {
-  const frames = []
-  
-  for(let i = 0; i < BOARD_HEIGHT - 1; i++) {
-    for(let j = 0; j < BOARD_WIDTH - 1; j++) {
-      // calculate positions
-      const nw_index = (BOARD_WIDTH*i)+j;
-      const ne_index = nw_index + 1;
-      const sw_index = nw_index + BOARD_WIDTH;
-      const se_index = sw_index + 1;
-  
-      const nw_value = board[nw_index];
-      const ne_value = board[ne_index];
-      const sw_value = board[sw_index];
-      const se_value = board[se_index];
-      
-      // lookup values
-      const frame = {
-        nw: {
-          value: nw_value,
-          index: nw_index
-        },
-        ne: {
-          value: ne_value,
-          index: ne_index
-        },
-        sw: {
-          value: sw_value,
-          index: sw_index
-        },
-        se: {
-          value: se_value,
-          index: se_index
-        }
-      }
-  
-      const frame_is_valid = frame => 
-        (frame.nw.value >= 0 && frame.nw.value < 6) &&
-        (frame.ne.value >= 0 && frame.ne.value < 6) &&
-        (frame.sw.value >= 0 && frame.sw.value < 6) &&
-        (frame.se.value >= 0 && frame.se.value < 6) &&
-        (frame.nw.value == 0 || frame.ne.value == 0 || frame.sw.value == 0 || frame.se.value == 0)
-      
-      if(frame_is_valid(frame)) {
-        frames.push(frame);
-      }
-    }  
-  }
-
-  const matches_frame = frame => {
-    return pattern => {
-      return frame.nw.value == 0 | frame.nw.value == pattern.nw &&
-      frame.ne.value == 0 | frame.ne.value == pattern.ne &&
-      frame.sw.value == 0 | frame.sw.value == pattern.sw &&
-      frame.se.value == 0 | frame.se.value == pattern.se
-    } 
-  }
-
-  function getRandom(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
-  }
-
-  const sort_bias_desc = (a, b) => {	  
-	return (selections[a.id] || 0) - (selections[b.id] || 0);
-  }
-	
-  const has_entropy = a => a.entropy > 0;
-  const sort_entropy_asc = (a, b) => a.entropy - b.entropy;
-  const calculate_entropy = frame => {
-    const matches = expanded_patterns.filter(matches_frame(frame));
-	frame.matches = matches;
-    frame.entropy = matches.length;	
-    return frame;
-  };
-  
-  const candidates = frames.map(calculate_entropy).filter(has_entropy).sort(sort_entropy_asc);
-    const selected = candidates[0];
-
-  if(selected === undefined) return board;
-    
-  const matches = shuffle(selected.matches).sort(sort_bias_desc);
-  const match = matches[0];
-     
-  nw_index = selected.nw.index;
-  ne_index = selected.ne.index;
-  sw_index = selected.sw.index;
-  se_index = selected.se.index;
-  
-  nw_value = match.nw;
-  ne_value = match.ne;
-  sw_value = match.sw;
-  se_value = match.se;
-  
-  board[nw_index] = nw_value;
-  board[ne_index] = ne_value;
-  board[sw_index] = sw_value;
-  board[se_index] = se_value;
-
-  selections[match.id] = 1 + (selections[match.id] || 0);
-
-  return board;
-}
-
-const calculate_bias = pattern => (new Set([pattern.nw, pattern.ne, pattern.sw, pattern.se])).size
-const add_bias = match => {
-  match.bias = calculate_bias(match);
-  return match;
-}
-
-let expanded_patterns = base_patterns.flatMap(expand_pattern_color).flatMap(expand_pattern_rotate).map(add_bias);
+let expanded_patterns = base_patterns.flatMap(expand_pattern_color).flatMap(expand_pattern_rotate);
 
 const feature_patterns = base_patterns.map(pattern => color_pattern(COLOR_UNKNOWN, pattern));
 
-const selections = new Array(base_patterns.length);
+const id_counter = new Array(base_patterns.length);
 
-function shuffle(array) {
+const shuffle = array => {
   let currentIndex = array.length,  randomIndex;
 
   // While there remain elements to shuffle.
   while (currentIndex > 0) {
 
     // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
+    randomIndex = Math.floor(random() * currentIndex);
     currentIndex--;
 
     // And swap it with the current element.
@@ -221,6 +227,7 @@ function shuffle(array) {
 
 let board = base_board;
 
+// iterate through the boards
 for(let i = 0; i < 200; i++) {
   expanded_patterns = shuffle(expanded_patterns);
   board = next_board(board);
@@ -252,7 +259,7 @@ feature_patterns.map(p => {
 	features_element.append(li);
 })
 
-console.log(selections);
+console.log(id_counter);
 console.log(expanded_patterns);
   
 })();
